dbcbfd33c15ea116ad0680c12b162895ccf21509
PolaPFA <38529757+PolaPFA@users.noreply.github.com>
2020-02-01 19:40:28 +0200

    Create PreparingData.py

---
+++ PreparingData.py
@@ -0,0 +1,27 @@
+import csv
+import pickle
+import pandas
+
+#Ser the dataset path here (.dat) files
+mypath = "E:\\College\\Graduation Project\\Dataset\\DEAP Dataset\\data_preprocessed_python\\data\\"
+
+#Converting each file to csv file
+for i in range(1,33):
+    name = ""
+    if i < 10:
+        name = "s0" + str(i)
+    else:
+        name = "s" + str(i)
+    print(name)
+    f = open(mypath+name+".dat", 'rb')
+    data = pickle.load(f, encoding='latin1')
+    labels = data["labels"]
+    for j in range(4):
+        Y = labels[i-1][j]
+        Y = pandas.Series(Y)
+        Y.to_csv('convertedData\\label'+str(j)+'.csv', mode='a', index=False)
+    for k in range(40):
+        X = data["data"][k][:][:]
+        X = pandas.DataFrame(X)
+        X.to_csv('convertedData\\'+name+'.csv', mode='a', index=False)
+
